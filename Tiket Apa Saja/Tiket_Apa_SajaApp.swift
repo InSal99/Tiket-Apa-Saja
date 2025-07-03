620d38c20e7d6619f584faeffa9c7b68f7b30a34
Intan Saliya <intansaliya99@gmail.com>
2025-07-03 11:17:09 +0700

    Initial Commit

---
+++ Tiket Apa Saja/Tiket_Apa_SajaApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tiket_Apa_SajaApp.swift
+//  Tiket Apa Saja
+//
+//  Created by Intan Saliya Utomo on 03/07/25.
+//
+
+import SwiftUI
+
+@main
+struct Tiket_Apa_SajaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Tiket Apa SajaTests/Tiket_Apa_SajaTests.swift
@@ -0,0 +1,17 @@
+//
+//  Tiket_Apa_SajaTests.swift
+//  Tiket Apa SajaTests
+//
+//  Created by Intan Saliya Utomo on 03/07/25.
+//
+
+import Testing
+@testable import Tiket_Apa_Saja
+
+struct Tiket_Apa_SajaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
