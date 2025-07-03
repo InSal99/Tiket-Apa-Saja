92e6a6e8df299166d3b838740628e84832ede0fe
Intan Saliya <intansaliya99@gmail.com>
2025-07-03 11:17:09 +0700

    Initial Commit

---
+++ Tiket Apa Saja/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Tiket Apa Saja
+//
+//  Created by Intan Saliya Utomo on 03/07/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
