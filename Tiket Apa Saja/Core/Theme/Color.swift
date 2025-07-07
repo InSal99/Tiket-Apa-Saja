b5e8f67d84245e9d47e8819e0fe8714627e6b75c
Intan Saliya <intansaliya99@gmail.com>
2025-07-07 17:18:21 +0700

    add: color

--- Tiket Apa Saja/Core/Theme/Color.swift
+++ Tiket Apa Saja/Core/Theme/Color.swift
@@ -5,3 +5,39 @@
 //  Created by Intan Saliya Utomo on 07/07/25.
 //
 
+import SwiftUI
+
+struct AppColor {
+    
+    struct Base {
+        static let white = Color("White")
+        static let black = Color("Black")
+    }
+    
+    struct Gray {
+        static let gray1 = Color("Gray-1")
+        static let gray2 = Color("Gray-2")
+        static let gray3 = Color("Gray-3")
+        static let gray4 = Color("Gray-4")
+        static let gray5 = Color("Gray-5")
+        static let gray6 = Color("Gray-6")
+        static let gray7 = Color("Gray-7")
+        static let gray8 = Color("Gray-8")
+        static let gray9 = Color("Gray-9")
+        static let gray10 = Color("Gray-10")
+    }
+    
+    struct Orange {
+        static let orange1 = Color("Orange-1")
+        static let orange2 = Color("Orange-2")
+        static let orange3 = Color("Orange-3")
+        static let orange4 = Color("Orange-4")
+        static let orange5 = Color("Orange-5")
+        static let orange6 = Color("Orange-6")
+        static let orange7 = Color("Orange-7")
+        static let orange8 = Color("Orange-8")
+        static let orange9 = Color("Orange-9")
+        static let orange10 = Color("Orange-10")
+    }
+    
+}
