c18474dd6038f5ac9c6804e8d94fd0f888b921a5
Intan Saliya <intansaliya99@gmail.com>
2025-07-20 19:59:51 +0700

    add: home page mvvm

--- Tiket Apa Saja/Features/Home/Model/Entertainment.swift
+++ Tiket Apa Saja/Features/Home/Model/Entertainment.swift
@@ -5,3 +5,15 @@
 //  Created by Intan Saliya Utomo on 07/07/25.
 //
 
+import SwiftUI
+
+struct Entertainment: Identifiable {
+    let id = UUID()
+    let logo: String
+    var isSubscribed: Bool
+    
+    init(icon: String, isSubscribed: Bool) {
+        self.logo = icon
+        self.isSubscribed = isSubscribed
+    }
+}
--- Tiket Apa Saja/Features/Home/ViewModel/EntertainmentViewModel.swift
+++ Tiket Apa Saja/Features/Home/ViewModel/EntertainmentViewModel.swift
@@ -5,3 +5,29 @@
 //  Created by Intan Saliya Utomo on 07/07/25.
 //
 
+import SwiftUI
+
+class EntertainmentViewModel: ObservableObject {
+    @Published var entertainments: [Entertainment] = []
+    
+    init() {
+        loadEntertainments()
+    }
+    
+    private func loadEntertainments() {
+        entertainments = [
+            Entertainment(icon: "banner1", isSubscribed: false),
+            Entertainment(icon: "banner1", isSubscribed: true),
+            Entertainment(icon: "banner1", isSubscribed: false),
+            Entertainment(icon: "banner1", isSubscribed: false)
+        ]
+    }
+    
+    func toggleSubscription(for id: UUID) {
+        entertainments.indices.forEach { index in
+            if entertainments[index].id == id {
+                entertainments[index].isSubscribed.toggle()
+            }
+        }
+    }
+}
